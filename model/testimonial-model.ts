36c63a557959236b9795972f48a7ffdf28092487
Nur A Alam <nuraalam376@gmail.com>
2024-09-22 23:23:10 +0600

    Category, testimonial, user and course model implementation

---
+++ model/testimonial-model.ts
@@ -0,0 +1,22 @@
+import mongoose, { Schema } from "mongoose";
+
+const testimonialSchema = new Schema({
+    content: {
+        required: true,
+        type: String
+    },
+    user: {
+        required: true,
+        type: String
+    },
+    courseId: {
+        required: true,
+        type: String
+    },
+    rating: {
+        required: true,
+        type: String
+    }
+});
+
+export const Testimonial = mongoose.models.Testimonial ?? mongoose.model("Testimonial", testimonialSchema);
--- queries/courses.ts
+++ queries/courses.ts
@@ -1,6 +1,18 @@
+import { Category } from "@/model/category-model";
 import { Course } from "@/model/course-model";
+import { Testimonial } from "@/model/testimonial-model";
+import { User } from "@/model/user-model";
 
 export async function getCourses() {
-    const courses = await Course.find({});
+    const courses = await Course.find({}).populate({
+        path: "category",
+        model: Category
+    }).populate({
+        path: "instructor",
+        model: User
+    }).populate({
+        path: "testimonials",
+        model: Testimonial
+    });
     return courses;
 }
